e3ff9256de5816753273439167ad04531b4c392d
RichardF12 <37163455+RichardF12@users.noreply.github.com>
2018-03-14 19:45:51 -0600

    Add files via upload

---
+++ Info.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info;
+
+/**
+ *
+ * @author ricky
+ */
+public class Info {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Nombres: Ricardo Jose");
+        System.out.println("Apellidos: Perez Montes");
+        System.out.println("Edad: 18");
+        System.out.println("Fecha de nacimiento: 03/04/1999");
+        System.out.println("Carne: 00168117");
+        System.out.println("Carrera: Ingenieria Informatica");
+        System.out.println("Color Favorito: Burgundy");
+    }
+    
+}
